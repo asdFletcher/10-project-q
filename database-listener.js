258141e5f614ea59392be4145c09bfa4a4d25d0c
Fletcher LaRue <laruefletcher@gmail.com>
2019-01-04 18:58:07 -0800

    connects some stuff up

---
+++ database-listener.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Q = require('./lib/subscriber.js');
+
+const db = new Q('database');
+
+// db.subscribe('delete');
+
+db.subscribe('delete', (payload) => {
+  console.log('delete happened', payload);
+});
+
+db.subscribe('create', (payload) => {
+  console.log('create happened', payload);
+});
+
+// console.log(db.subscriptions());
