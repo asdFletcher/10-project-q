0d1f2fceaa9f048d61e5eea25affb6e479705570
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-01-06 11:52:59 -0800

    adds passing tests and updates readme

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,41 +1,72 @@
 'use strict';
 
 // import stuff, simulator, qserver, listener
+const Publisher = require('../lib/publisher.js');
+const Subscriber = require('../lib/subscriber.js');
+const Server = require('../lib/server.js');
 
-// describe tests
-describe('the server', () => {
-
-  it('does stuff', ()=> {
-    // publisher
-    const Publisher = require('./lib/publisher.js');
+describe('publisher', () => {
+  it('emits publisher events', () => {
     const publisher = new Publisher;
-    
-    // server
-    const server = require('./lib/server.js');
-    server.start();
-    const serverDB = new Server('database');
-    serverDB.monitorEvent('create');
-    serverDB.monitorEvent('update');
-    serverDB.monitorEvent('delete');
-    
-    // subscriber
-    const Subscriber = require('./lib/subscriber.js');
-    const subscriber = new Subscriber('database');
-    subscriber.subscribe('delete', (payload) => {
-      console.log('delete happened', payload);
-    });
-
-    // expect to see an event come out of here
-    Q.publish('database', 'delete', {id:77});
-    
-    // expect the server to recevie that event
-    // mystery  code here
-
-    // expect the subscriber to see the event
-    // mysterey code here
-
-
-
+    let spy = jest.spyOn(console, 'log');
+    publisher.publish();
+    expect(spy).toHaveBeenCalled();
   });
-
 });
+
+describe('subsciber', () => {
+  it('emits subscriber events', () => {
+    const subscriber = new Subscriber('database');
+    let spy = jest.spyOn(console, 'log');
+    subscriber.subscribe();
+    expect(spy).toHaveBeenCalled();
+  });
+});
+
+// describe('server', () => {
+//   it('handles room joins', () => {
+//     const serverDB = new Server('database');;
+//     let spy = jest.spyOn(console, 'log');
+//     expect(spy).toHaveBeenCalled();
+//   });
+// });
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// // describe tests
+// describe('the server', () => {
+
+//   it('does stuff', ()=> {
+//     // publisher
+//     const Publisher = require('./lib/publisher.js');
+//     const publisher = new Publisher;
+    
+//     // server
+    // const server = require('./lib/server.js');
+    // server.start();
+    // const serverDB = new Server('database');
+//     serverDB.monitorEvent('create');
+//     serverDB.monitorEvent('update');
+//     serverDB.monitorEvent('delete');
+    
+//     // subscriber
+    // const Subscriber = require('./lib/subscriber.js');
+    // const subscriber = new Subscriber('database');
+//     subscriber.subscribe('delete', (payload) => {
+//       console.log('delete happened', payload);
+//     });
+
+//     // expect to see an event come out of here
+//     Q.publish('database', 'delete', {id:77});
+    
+//     // expect the server to recevie that event
+//     // mystery  code here
+
+//     // expect the subscriber to see the event
+//     // mysterey code here
+
+
+
+//   });
+
+// });
